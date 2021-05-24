23149cf0fca0522e447ecdad1abbedb4828e3833
Vinhh <vinhvipvl@gmail.com>
2021-05-24 21:21:03 +0700

    complete pen

--- src/com/demo/shape/Pen.java
+++ src/com/demo/shape/Pen.java
@@ -16,6 +16,8 @@ public class Pen extends Geometry {
     private Line line;
     private Point2D p1, p2;
 
+    private boolean[][] board = new boolean[DrawCanvas.rowSize][DrawCanvas.colSize];
+
     public Pen(DrawCanvas canvas) {
         super(canvas);
         line = new Line(canvas, DrawMode.DEFAULT, color);
@@ -46,22 +48,19 @@ public class Pen extends Geometry {
     @Override
     public void draw() {
         processDraw();
+        drawNewPoints();
     }
 
     @Override
     public void processDraw() {
-        if (listDraw.size() >= 1) {
-            if (endPoint != null && !endPoint.isNear(listDraw.get(listDraw.size() - 1))) {
-                endPoint.setColor(color);
-                listDraw.add(endPoint);
-
-                line.setStartPoint(listDraw.get(listDraw.size() - 2));
-                line.setEndPoint(listDraw.get(listDraw.size() - 1));
-                line.processDraw();
-                line.drawNewPoints();
-
-                for (int i = 1; i < line.getListDraw().size() - 1; i++) {
-                    listDraw.add(listDraw.size() - 1, line.getListDraw().get(i));
+        listDraw.clear();
+        Point2D p;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j]) {
+                    p = Point2D.fromComputerCoordinate(i, j);
+                    p.setColor(color);
+                    listDraw.add(p);
                 }
             }
         }
@@ -80,15 +79,27 @@ public class Pen extends Geometry {
         super.setStartPoint(startPoint);
         if (startPoint != null) {
             startPoint.setColor(color);
-            listDraw.add(startPoint);
-            drawNewPoints();
-            showPointsCoordinate();
+            p2 = startPoint;
+            board[startPoint.getComputerX()][startPoint.getComputerY()] = true;
         }
     }
 
     @Override
     public void setEndPoint(Point2D endPoint) {
         super.setEndPoint(endPoint);
+        if (endPoint != null) {
+            p1 = p2;
+            p2 = endPoint;
+            board[p2.getComputerX()][p2.getComputerY()] = true;
+            if (!p1.isNear(p2)) {
+                line.setStartPoint(p1);
+                line.setEndPoint(p2);
+                line.processDraw();
+                for (Point2D p : line.getListDraw()) {
+                    board[p.getComputerX()][p.getComputerY()] = true;
+                }
+            }
+        }
     }
 
     public void setListDraw(List<Point2D> list) {
